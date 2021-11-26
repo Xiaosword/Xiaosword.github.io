026a6a77708aaee1feb6c31f3f7399bd62e9ed9f
sword <sword@swordMacBookPro.local>
2021-11-26 14:12:57 +0800

    update: config

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,12 +1,12 @@
-export const dateFormat = (endDate: any) => {
-  let start: any = new Date()
-  let end: any = new Date(endDate)
+export const dateFormat = (endDate: string) => {
+  let start: number = new Date().valueOf()
+  let end: number = new Date(endDate).valueOf()
   let xtime = start - end
   let xdays = Math.floor(xtime / 86400000)
   let xhour = Math.floor((xtime % 86400000) / 3600000)
   let xmin = Math.floor(((xtime % 86400000) % 3600000) / 60000)
   let xsec = Math.floor((((xtime % 86400000) % 3600000) % 60000) / 1000)
-  return xdays + '天' + xhour + '时' + fillZero(xmin) + '分' + fillZero(xsec) + '秒' 
+  return xdays + '天' + xhour + '时' + fillZero(xmin) + '分' + fillZero(xsec) + '秒'
 }
 
 const fillZero = (nums: number) => {
